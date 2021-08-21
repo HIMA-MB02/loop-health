9d0832adb671d682281935b287d03fd042a1466a
HIMA-MB02 <himanshu.ganpa@gmail.com>
2021-08-21 15:42:49 +0530

    changes

--- src/components/atoms/Navbar/index.tsx
+++ src/components/atoms/Navbar/index.tsx
@@ -7,6 +7,14 @@ const Navbar: React.FunctionComponent = () => {
             <div className='container-fluid'>
                 <div className='navbar-brand'>Dashboard</div>
             </div>
+            <div className='search-bar'>
+                <i className='fa fa-search fa-icon-position'></i>
+                <input
+                    type='text'
+                    className='search-control form-control'
+                    placeholder='Search for a brand, product or category'
+                />
+            </div>
         </nav>
     );
 };
